a4b3744ad528d3f9ff53c92053365551148323ae
Ajith <ajithpkumar231@gmail.com>
2025-09-02 22:20:07 +0530

    wallet adapter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
 import LaunchPad from './LaunchPad'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
 
   return (
     <>
-      <LaunchPad />
+      <ConnectionProvider endpoint='https://api.devnet.solana.com'>
+        <WalletProvider wallets={[]}>
+          <WalletModalProvider>
+            <WalletMultiButton />
+            <LaunchPad />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   )
 }
--- src/LaunchPad.tsx
+++ src/LaunchPad.tsx
@@ -1,12 +1,35 @@
+import { useState, type ChangeEvent } from "react"
+type Value = {
+    name: string,
+    symbol: string,
+    url: string,
+    supply: string
+}
+
 const LaunchPad = () => {
+    const [values, setValues] = useState<Value>({
+        name: "",
+        symbol: "",
+        url: "",
+        supply: ""
+    })
+
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+
+    const handleSubmit = () => {
+        console.log(values);
+    }
     return (
-        <div>
+        <div className="">
             <h1>Solana Token Launchpad</h1>
-            <input className='inputText' type='text' placeholder='Name'></input> <br />
-            <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-            <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-            <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-            <button className='btn'>Create a token</button>
+            <input onChange={handleChange} name="name" className='inputText' type='text' placeholder='Name'></input> <br />
+            <input onChange={handleChange} name="symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
+            <input onChange={handleChange} name="url" className='inputText' type='text' placeholder='Image URL'></input> <br />
+            <input onChange={handleChange} name="supply" className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+            <button onClick={handleSubmit} className='btn'>Create a token</button>
         </div>
     )
 }
